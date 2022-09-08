e98c98f7ce77f94b5c1cb9e04c66e27dcec345a4
Antonius131 <antomendetta@gmail.com>
2022-09-08 15:39:40 +0200

    create index() to return the index in the view

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        return view('movies.index');
+    }
 }
